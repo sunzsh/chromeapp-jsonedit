a95f6ff04edf4652c50a81e75a43f50a1c32188e
sungf <c.2012@foxmail.com>
2022-03-09 22:25:33 +0800

    up his

--- resources/main.js
+++ resources/main.js
@@ -1,10 +1,24 @@
 const container = document.getElementById('jsoneditor')
-
+const jsonkey = 'jsonv'
   const options = {
     mode: 'code',
-    modes: ['code', 'form', 'text', 'tree', 'view', 'preview'] // allowed modes
+    modes: ['code', 'form', 'text', 'tree', 'view', 'preview'], // allowed modes
+    onChange: () => {
+      try {
+        localforage.setItem(jsonkey,editor.get())
+      } catch (e) {
+      }
+    },
+    
   }
-
-  const json = { }
-
-  const editor = new JSONEditor(container, options, json)
+ 
+let json={}
+let editor
+async function init() {
+  try {
+    json = await localforage.getItem(jsonkey)
+  } catch (e) {
+  }
+  editor = new JSONEditor(container, options, json)
+}
+init()
